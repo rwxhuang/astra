bf1a03f29a9e5c28d957f385cbc282ff5b737f50
rwxhuang <rwxhuang@gmail.com>
2024-12-17 11:24:11 -0800

    Added requirements.txt, connected to s3 bucket

--- pages/data.py
+++ pages/data.py
@@ -1,12 +1,14 @@
 import streamlit as st
 from pygwalker.api.streamlit import StreamlitRenderer
+from st_files_connection import FilesConnection
 import pandas as pd
 
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed", page_icon='🛰️')
 
 st.header("Technology Project Data")
 
-df = pd.read_csv("./data/techport_all.csv")
+conn = st.connection('s3', type=FilesConnection)
+df = conn.read("astra-data-bucket/techport_all.csv", input_format="csv", ttl=600)
  
 pyg_app = StreamlitRenderer(df)
  
