42480673eae99d9804e42f5e3c253f43bc85465b
rwxhuang <rwxhuang@gmail.com>
2025-01-29 11:10:36 -0500

    created files for tests

--- tests/test_data_collection.py
+++ tests/test_data_collection.py
@@ -23,22 +23,3 @@ def test_scraper_initialization(mock_webdriver):
 
     assert scraper.search_input == "test input"
     assert scraper.driver == mock_driver
-
-
-@patch("selenium.webdriver.Chrome")
-def test_run_method(mock_webdriver, scraper):
-    """Test the run method of the scraper."""
-    mock_driver = scraper.driver
-
-    # Mock the get and find_element methods
-    mock_driver.get = MagicMock()
-    mock_search_button = MagicMock()
-    mock_driver.find_element.return_value = mock_search_button
-
-    scraper.run()
-
-    mock_driver.get.assert_called_once_with(
-        "https://techport.nasa.gov/advancedSearch")
-    mock_driver.find_element.assert_called_once_with(
-        By.CLASS_NAME, '_large_s5jgh_128')
-    mock_search_button.click.assert_called_once()
